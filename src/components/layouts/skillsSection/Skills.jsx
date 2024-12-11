7d1a7d2394005cb72d3e9e80884d9a238e0b781b
Camilo <camilomedina.es@gmail.com>
2024-12-11 20:56:37 -0300

    animaciones para la entrada de las paginas, cambio de logo del idioma.

--- src/components/layouts/skillsSection/Skills.jsx
+++ src/components/layouts/skillsSection/Skills.jsx
@@ -6,7 +6,7 @@ import AvatarPortrait2 from "../../common/avatars/AvatarPortrait2";
 
 const Skills = ({ content }) => {
   return (
-    <div className="bg-gradient-to-b from-base-100 via-base-300 to-base-100 h-fit flex flex-col items-center space-y-6">
+    <section className="bg-gradient-to-b from-base-100 via-base-300 to-base-100 h-fit flex flex-col items-center space-y-6 transition ease-in-out animate-fade-in">
       <div className="flex items-center space-x-4">
         <h2 className="text-3xl text-center lg:text-5xl">{content.title1}</h2>
         <AvatarPortrait />
@@ -21,7 +21,7 @@ const Skills = ({ content }) => {
       <div className="w-full max-w-xl">
         <AccordionProyects content={content} />
       </div>
-    </div>
+    </section>
   );
 };
 
