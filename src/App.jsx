c930fdb478d7f7369daaadc7c2f909c3a1372f1f
Camilo <camilomedina.es@gmail.com>
2025-06-11 20:59:34 -0400

    primer commit porta nuevo

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,12 @@
-import NavbarContainer from "./components/layouts/navbar/NavbarContainer";
-import FooterContainer from "./components/layouts/footer/FooterContainer";
-import AboutPage from "./components/pages/about/AboutPage";
-import HomePage from "./components/pages/homeContainer/HomePage";
-import SkillsPage from "./components/pages/skills/SkillsPage";
-import Page404 from "./components/pages/404/Page404";
-import { Routes, BrowserRouter, Route } from "react-router-dom";
-import { LanguageProvider } from "./context/LanguageContext";
-import ContactPage from "./components/pages/contact/ContactPage";
-import { AlertProvider } from "./context/AlertContext";
-
+import Hero from "./components/sections/Hero";
 function App() {
   return (
-    <LanguageProvider>
-      <BrowserRouter>
-        <AlertProvider>
-          <NavbarContainer />
-          <Routes>
-            <Route path={"/"} element={<HomePage />}></Route>
-            <Route path={"/About"} element={<AboutPage />}></Route>
-            <Route path={"/Skills"} element={<SkillsPage />}></Route>
-            <Route path={"/Contact"} element={<ContactPage />}></Route>
-            <Route path={"*"} element={<Page404 />} />
-          </Routes>
-          <FooterContainer />
-        </AlertProvider>
-      </BrowserRouter>
-    </LanguageProvider>
+    <>
+      <header>{/* <Navbar /> */}</header>
+      <main>
+        <Hero />
+      </main>
+    </>
   );
 }
 
---
+++ src/constants/index.js
@@ -0,0 +1,10 @@
+export const words = [
+  { text: "Ideas", imgPath: "/images/ideas.svg" },
+  { text: "Conceptos", imgPath: "/images/concepts.svg" },
+  { text: "Diseños", imgPath: "/images/designs.svg" },
+  { text: "Código", imgPath: "/images/code.svg" },
+  { text: "Ideas", imgPath: "/images/ideas.svg" },
+  { text: "Conceptos", imgPath: "/images/concepts.svg" },
+  { text: "Diseños", imgPath: "/images/designs.svg" },
+  { text: "Código", imgPath: "/images/code.svg" },
+];
