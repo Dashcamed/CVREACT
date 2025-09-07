d81cf07bfc634685e348e094453113e0a77a39d5
Camilo <camilomedina.es@gmail.com>
2025-09-07 10:15:50 -0300

    Modificacion cambio de chunks

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,26 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    // 👇 sube el límite del warning (opcional, evita spam en consola)
+    chunkSizeWarningLimit: 2000,
+
+    // 👇 divide dependencias pesadas en chunks separados
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ["react", "react-dom"],
+          three: [
+            "three",
+            "@react-three/fiber",
+            "@react-three/drei",
+            "@react-three/postprocessing",
+          ],
+          gsap: ["gsap", "@gsap/react"],
+        },
+      },
+    },
+  },
 });
